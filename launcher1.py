b6b4db4ee1b5d9bd360b1e43a3740d06dec0b6c1
Mateusz Kowalski <mateusz.kowalski@cern.ch>
2017-02-02 00:55:13 +0100

    Basic wrapper around tools

---
+++ launcher1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+__author__ = "Mateusz Kowalski"
+__email__ = "mateusz.kowalski@cern.ch"
+__status__ = "Development"
+
+import tools
+
+if __name__ == "__main__":
+    tools.do_heavy_computation(time=1)
+    tools.do_nothing(time=1)
+    tools.read_from_file('/etc/hosts')
+    tools.do_heavy_computation(time=3)
+    tools.do_nothing(time=3)
+    tools.run_application("echo", "test 123")
